868b489b96f8d3ec6a1f792d7cca19b4eef4f427
Moses Surumen <moses.susrumen@gmail.com>
2023-11-19 14:05:16 -0800

    player and team stats

---
+++ client/scrapers/whoscored/team/__init__.py
@@ -0,0 +1,4 @@
+from client.scrapers.whoscored.team.overall import crawl_team_overall_stats
+from client.scrapers.whoscored.team.defensive import crawl_team_defensive_stats
+from client.scrapers.whoscored.team.offensive import crawl_team_offensive_stats
+
